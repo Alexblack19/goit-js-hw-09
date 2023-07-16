2afcb55dc194aae893d3969eab3beb23a297b261
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-16 19:42:44 +0000

    Deploying to gh-pages from @ Alexblack19/goit-js-hw-09@ef515d99d1ad1d31e382450dd16da9cd4e4b4796 🚀

---
+++ 01-color-switcher.e40d46d6.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(function(e){e.target.setAttribute("disabled",""),o=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=e}),1e3)})),n.addEventListener("click",(function(t){clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.e40d46d6.js.map
