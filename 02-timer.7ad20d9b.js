6d319681046b89818411a09ca6c3640695dc3912
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-19 15:24:21 +0000

    Deploying to gh-pages from @ Alexblack19/goit-js-hw-09@7b1f85a9e429acc2d22a66752931f27d202d84ce 🚀

---
+++ 02-timer.7ad20d9b.js
@@ -0,0 +1,2 @@
+!function(){var o=new Date;console.log(o),console.log(o.getFullYear()),console.log(o.getHours()),console.log(o.getTime()),console.log(o.getDay()),console.log(o.getMonth())}();
+//# sourceMappingURL=02-timer.7ad20d9b.js.map
---
+++ 02-timer.e7f4ea02.js
@@ -0,0 +1,2 @@
+const o=new Date;console.log(o),console.log(o.getFullYear()),console.log(o.getHours()),console.log(o.getTime()),console.log(o.getDay()),console.log(o.getMonth());
+//# sourceMappingURL=02-timer.e7f4ea02.js.map
