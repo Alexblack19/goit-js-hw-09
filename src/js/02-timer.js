d85b51f2cd2d422fad2c678f9f8cf0bfb35374d3
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-19 19:20:47 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,40 @@
 // Описаний в документації
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 
 const timer = {
   start() {
     const startTime = Date.now();
     setInterval(() => {
-      const currentTime = Date.now();      
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const timerComponents = convertMs(deltaTime);
+      console.log(timerComponents);
     }, 1000);
   },
 };
 
 timer.start();
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
