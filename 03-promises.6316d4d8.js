07108b2c959fcff8b42615e2df42c3bf64fda2b8
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-21 12:52:25 +0000

    Deploying to gh-pages from @ Alexblack19/goit-js-hw-09@cbf6ac3072c9120221267a32cea78ca91f2c92c8 🚀

--- 03-promises.6316d4d8.js
+++
@@ -1,2 +0,0 @@
-function e(e,t){Math.random()}document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault(),console.dir(t);const n=t.target.elements.delay.value,r=t.target.elements.step.value,a=t.target.elements.amount.value;for(let t=0;t<Number(a);t+=1)e(n,r);t.currentTarget.reset()}));
-//# sourceMappingURL=03-promises.6316d4d8.js.map
---
+++ 03-promises.bb5ae3fb.js
@@ -0,0 +1,2 @@
+function e(e,t){Math.random()}document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault();const n=t.target.elements.delay.value,r=t.target.elements.step.value,a=t.target.elements.amount.value;for(let t=0;t<Number(a);t+=1)e(n,r);t.currentTarget.reset()}));
+//# sourceMappingURL=03-promises.bb5ae3fb.js.map
