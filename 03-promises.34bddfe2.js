15e8ce7d7207a044644fe29cd239f653de54ed20
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-21 13:52:17 +0000

    Deploying to gh-pages from @ Alexblack19/goit-js-hw-09@8d08f4608653527e3db4c8c3649a40ef1bbab6e1 🚀

--- 03-promises.34bddfe2.js
+++
@@ -1,2 +0,0 @@
-!function(){function e(e,t){Math.random()}document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault();for(var n=t.target.elements.delay.value,r=t.target.elements.step.value,a=t.target.elements.amount.value,u=0;u<Number(a);u+=1)e(n,r);t.currentTarget.reset()}))}();
-//# sourceMappingURL=03-promises.34bddfe2.js.map
