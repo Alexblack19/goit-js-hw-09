cf1943f6766da747aec22d17c57224a5dab0bf67
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-21 15:50:52 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,20 @@
-function createPromise(position, delay) {
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {  
+  e.preventDefault();
+  console.dir(e);
+  const position = e.target.elements.delay.value;
+  const delay = e.target.elements.step.value;
+  const amount = e.target.elements.amount.value; 
+  for (let i = 0; i < Number(amount); i += 1) {
+    createPromise(position, delay);
+  }
+  e.currentTarget.reset();
+}
+
+function createPromise(position, delay) { 
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
