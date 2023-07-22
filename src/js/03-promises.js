29e4ad5add74cdb80a345a793056e405d1a6d06c
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-07-22 17:10:24 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -28,19 +28,29 @@ function onSubmit(e) {
   const amount = Number(e.target.elements.amount.value);
 
   for (let position = 1; position <= amount; position += 1) {
-    console.log(position, delay);
-    setTimeout(createPromise(position, delay), delay);
+    createPromise(position, delay);
     delay = delay + step;
   }
 }
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    // Reject
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('Success! Value passed to resolve function');
+      } else {
+        reject('Error! Error passed to reject function');
+      }
+    }, delay);
+  });
 }
+
+createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
